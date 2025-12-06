e811fbd02ee3b079b01ee30263269d7a9e94c1fc
0ekekddddd <0ekekddddd@protonmail.com>
2025-12-06 08:26:15 -0600

    .

--- art/js.js
+++ art/js.js
@@ -1,4 +1,4 @@
 function guLogMap(){
-  document.write('<span style="text-align:center;"><a href="." target="_parent"><img src="ui/sdtitle.png" alt="*GRISTLEPOD*" style="max-width:180px;"></a></span><div><table><tr><th style="text-align:center;">navigation</th></tr><tr><td><a href="gallery" target="_parent">gallery</a></td></tr><tr><td><a href="sketches" target="_parent">sketches</a></td></tr><tr><td><a href="terms" target="_parent">terms</a></td></tr><tr><td class="recentlyUpdated"><a href="merchsamples" target="_parent">merch samples</a></td></tr><tr><td><a href="merch" target="_parent">offline merch</a></td></tr></table></div>');
+  document.write('<span style="text-align:center;"><a href="." target="_parent"><img src="ui/sdtitle.png" alt="*GRISTLEPOD*" style="max-width:180px;"></a></span><div><table><tr><th style="text-align:center;">navigation</th></tr><tr><td><a href="gallery" target="_parent">gallery</a></td></tr><tr><td><a href="sketches" target="_parent">sketches</a></td></tr><tr><td><a href="terms" target="_parent">terms</a></td></tr><tr><tr><td class="recentlyUpdated"><a href="merch" target="_parent">shop</a></td></tr></table></div>');
   eventWidget();
 }
