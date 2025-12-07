6f41b5872a38a5559207be1e9f5e20e9774da35e
0ekekddddd <0ekekddddd@protonmail.com>
2025-12-07 05:19:16 -0600

    .

--- art/js.js
+++ art/js.js
@@ -1,4 +1,4 @@
 function guLogMap(){
-  document.write('<span style="text-align:center;"><a href="." target="_parent"><img src="ui/sdtitle.png" alt="*GRISTLEPOD*" style="max-width:180px;"></a></span><div><table><tr><th style="text-align:center;">navigation</th></tr><tr><td><a href="gallery" target="_parent">gallery</a></td></tr><tr><td><a href="sketches" target="_parent">sketches</a></td></tr><tr><td><a href="terms" target="_parent">terms</a></td></tr><tr><tr><td class="recentlyUpdated"><a href="merch" target="_parent">shop</a></td></tr></table></div>');
+  document.write('<span style="text-align:center;"><a href="." target="_parent"><img src="ui/sdtitle.gif" alt="*GRISTLEPOD*" style="max-width:180px; image-rendering:crisp-edges;"></a></span><br><div class="altm1"><ol><li><a href="gallery" target="_parent">gallery</a></li><li><a href="sketches" target="_parent">sketches</a></li><li><a href="terms" target="_parent">terms</a></li><li><a href="merch" target="_parent">shop</a></li></ol></div>');
   eventWidget();
 }
