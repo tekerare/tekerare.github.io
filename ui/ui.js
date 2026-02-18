71443d880d7c87c1d57c26ccdd4cf207a30e20af
0ekekddddd <0ekekddddd@protonmail.com>
2026-02-17 23:14:33 -0600

    .

--- ui/ui.js
+++ ui/ui.js
@@ -41,7 +41,7 @@ function galleryWidget(){
 function eventWidget(){
   const x = document.createElement('div');
   x.setAttribute('id', 'eventsWidget');
-  x.innerHTML = '<h3>upcoming booths</h3><div id="eventsList"><quiet style="text-align:center;">nearest &rarr; distant</quiet><ul><li><a href="https://ccs-houston.com/events/artist-alley-houston-i/" target="_blank">artist alley houston</a><br>(jan 31 10am - 5pm)</li><li><a href="https://www.kimokawaii.net" target="_blank">kimokawaii</a> @ conroe tx<br>(june 06-07)</li><li><strike>zakicon @ friendswood tx<br>(jan 09-11)</strike></li></ul></div><p style="text-align:end;">virtual booth tour @ <a href="https://' + siteURL + '/art/merch" target="_parent">/art/</a></p>';
+  x.innerHTML = '<h3>upcoming booths</h3><div id="eventsList"><quiet style="text-align:center;">nearest &rarr; distant</quiet><ul><li><a href="https://ccs-houston.com/" target="_blank">artist alley houston part 2</a><br>(april 18 - 19)</li><li><a href="https://www.kimokawaii.net" target="_blank">kimokawaii</a> @ conroe tx<br>(june 06-07)</li><strike><li><a href="https://ccs-houston.com/events/artist-alley-houston-i/" target="_blank">artist alley houston</a><br>(jan 31 10am - 5pm)</li><li>zakicon @ friendswood tx<br>(jan 09-11)</strike></li></ul></div><p style="text-align:end;">virtual booth tour @ <a href="https://' + siteURL + '/art/merch" target="_parent">/art/</a></p>';
   document.getElementById('asideWrp').appendChild(x);
 }
 function kofiWidget(){
