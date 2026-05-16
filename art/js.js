2de3b0c05e8a492069bd528b3d11c9c015810e69
0ekekddddd <0ekekddddd@protonmail.com>
2026-05-15 23:03:39 -0500

    .

--- art/js.js
+++ art/js.js
@@ -1,8 +1,10 @@
 function guLogMap(){
-  document.write('<a href="https://' + siteURL + '/art/" target="_parent"><img src="https://' + siteURL + '/art/ui/sdtitle.gif" alt="*GRISTLEPOD*" style="max-width:180px; image-rendering:crisp-edges;"></a><br><div class="altm1"><ol><li><a href="https://' + siteURL + '/art/gallery" target="_parent">gallery</a></li><li><a href="https://' + siteURL + '/art/sketches" target="_parent">sketches</a></li><li><a href="https://' + siteURL + '/art/terms" target="_parent">terms</a></li><li><a href="https://' + siteURL + '/art/merch" target="_parent">shop</a></li><li><a href="https://' + siteURL + '/art/timeline" target="_parent">booth history</a></li></ol></div>');
+  document.write('<a href="https://' + siteURL + '/art/" target="_parent"><img src="https://' + siteURL + '/media/label/gristlepod_spin.gif" alt="*GRISTLEPOD*" style="max-width:180px; image-rendering:crisp-edges;"></a><br><div class="altm1"><ol><li><a href="https://' + siteURL + '/art/gallery" target="_parent">gallery</a></li><li><a href="https://' + siteURL + '/art/sketches" target="_parent">sketches</a></li></ol></div>');
   eventWidget();
 }
 window.onload = function(){
-  const locationTitle = document.getElementById('windowLocation');
-  locationTitle.innerHTML = siteURL + '/art/';
+  const x = document.createElement("span");
+  x.setAttribute("style", "text-align:center;");
+  x.innerHTML = '<img src="../media/label/gristlepod_180.png" alt="">';
+  document.getElementById('mainWrp').appendChild(x);
 }
---
+++ shop/js.js
@@ -0,0 +1,10 @@
+function guLogMap(){
+  document.write('<a href="https://' + siteURL + '/shop/" target="_parent"><img src="https://' + siteURL + '/art/ui/sdtitle.gif" alt="*GRISTLEPOD*" style="filter: drop-shadow(0 0 .2rem #ffffff);max-width:180px; image-rendering:crisp-edges;"></a><br><div class="altm1"><ol><li><a href="https://' + siteURL + '/art/" target="_parent">portfolio</a></li><li><a href="https://' + siteURL + '/shop/terms" target="_parent">terms</a></li><li><a href="https://' + siteURL + '/shop/timeline" target="_parent">booths</a></li></ol></div>');
+  eventWidget();
+}
+window.onload = function(){
+  const x = document.createElement("span");
+  x.setAttribute("style", "text-align:center;");
+  x.innerHTML = '<img src="../media/label/gristlepod_180.png" alt="">';
+  document.getElementById('mainWrp').appendChild(x);
+}
