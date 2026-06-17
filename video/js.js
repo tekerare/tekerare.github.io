a69e0efd6940d9b7067b2d5dd7a70d9b20accc73
tekerare <hijirihara@protonmail.com>
2026-06-17 08:14:52 -0500

    .

--- video/js.js
+++ video/js.js
@@ -1,4 +1,4 @@
 window.onload = function(){
   const x = document.getElementById('asideWrp');
-  x.innerHTML = '<span style="text-align:center;"><a href="."><img src="ui/sdtitle.png" alt=""></a></span><br><ul class="ulSiteMap"><li><a href="https://' + siteURL + '/video/" target="_parent">All Videos</a></li><li><a href="https://' + siteURL + '/video/stream/" target="_parent">Stream</a></li></ul>';
+  x.innerHTML = '<span style="text-align:center;"><a href="."><img src="../media/label/muleviewer.png" alt=""></a><a href="https://'+ siteURL +'/video/stream"><img src="../media/label/quitterspark.png" alt=""></a></span><br>';
 }
