a723c6ac4a23f3e854f3bb83d6eea5244f0e850e
tekerare <hijirihara@protonmail.com>
2026-06-19 05:52:16 -0500

    .

--- shop/js.js
+++ shop/js.js
@@ -1,9 +1,8 @@
-function guLogMap(){
-  document.write('<a href="https://' + siteURL + '/shop/" target="_parent"><img src="https://' + siteURL + '/art/ui/sdtitle.gif" alt="*GRISTLEPOD*" style="filter: drop-shadow(0 0 .2rem #ffffff);max-width:180px; image-rendering:crisp-edges;"></a>');
+window.onload = function(){
+  const y = document.getElementById('asideWrp');
+  y.innerHTML = '<a href="https://' + siteURL + '/shop/" target="_parent"><img src="https://' + siteURL + '/art/ui/sdtitle.gif" alt="*GRISTLEPOD*" style="filter: drop-shadow(0 0 .2rem #ffffff);max-width:180px; image-rendering:crisp-edges;"></a>';
   artSiteMap();
   eventWidget();
-}
-window.onload = function(){
   const x = document.createElement("span");
   x.setAttribute("style", "text-align:center;");
   x.innerHTML = '<img src="../media/label/gristlepod_180.png" alt="">';
