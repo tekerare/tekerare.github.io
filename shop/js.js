ffa144de3d007f1db6fb25fb17e2e1d770314507
tekerare <hijirihara@protonmail.com>
2026-08-11 01:16:28 -0500

    .

--- shop/js.js
+++ shop/js.js
@@ -1,6 +1,6 @@
 window.onload = function(){
   const y = document.getElementById('asideWrp');
-  y.innerHTML = '<a href="https://' + siteURL + '/shop/" target="_parent"><img src="https://' + siteURL + '/art/ui/sdtitle.gif" alt="*GRISTLEPOD*" style="filter: drop-shadow(0 0 .2rem #ffffff);max-width:180px; image-rendering:crisp-edges;"></a>';
+  y.innerHTML = '<a href="https://' + siteURL + '/shop/" target="_parent"><img src="https://' + siteURL + '/media/label/gristlepod_spin.gif" alt="*GRISTLEPOD*" style="filter: drop-shadow(0 0 .2rem #ffffff);max-width:180px; image-rendering:crisp-edges;"></a>';
   artSiteMap();
   eventWidget();
   const x = document.createElement("span");
