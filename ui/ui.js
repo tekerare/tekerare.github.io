c080c6177fdff6562ef146034d4d62eb0434d1c0
tekerare <hijirihara@protonmail.com>
2026-08-24 23:18:07 -0500

    .

--- ui/ui.js
+++ ui/ui.js
@@ -50,7 +50,7 @@ function galleryWidget(){
 function artSiteMap(){
   const x = document.createElement("ul");
   x.setAttribute('class','ulSiteMap');
-  x.innerHTML = '<li><a href="https://' + siteURL + '/art/">portfolio</a></li><ul><li><a href="https://' + siteURL + '/art/gallery" target="_parent">gallery</a></li><li><a href="https://' + siteURL + '/art/sketches" target="_parent">sketches</a></li></ul><li><a href="https://' + siteURL + '/shop/" target="_parent">shop</a></li><ul><li><a href="https://www.patreon.com/cw/tekerare" target="_blank">patreon</a></li><li><a href="https://' + siteURL + '/shop/terms" target="_parent">terms</a></li><li><a href="https://' + siteURL + '/shop/booths" target="_parent">my booths</a></li></ul><li></li>';
+  x.innerHTML = '<li><a href="https://' + siteURL + '/art/">portfolio</a></li><ul><li><a href="https://' + siteURL + '/art/gallery" target="_parent">gallery</a></li><li><a href="https://' + siteURL + '/art/sketches" target="_parent">sketches</a></li></ul><li><a href="https://' + siteURL + '/shop/" target="_parent">shop</a></li><ul><li><a href="https://' + siteURL + '/shop/testimonials" target="_parent">reviews</a></li><li><a href="https://www.patreon.com/cw/tekerare" target="_blank">patreon</a></li><li><a href="https://' + siteURL + '/shop/terms" target="_parent">terms</a></li><li><a href="https://' + siteURL + '/shop/booths" target="_parent">my booths</a></li></ul><li></li>';
   document.getElementById('asideWrp').appendChild(x);
 }
 function eventWidget(){
