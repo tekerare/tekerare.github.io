13a7d13f45800576090dfd45bd9f678fb229d8fa
tekerare <hijirihara@protonmail.com>
2026-09-08 18:16:22 -0500

    .

--- video/js.js
+++ video/js.js
@@ -1,4 +1,4 @@
 window.onload = function(){
   const x = document.getElementById('asideWrp');
-  x.innerHTML = '<span style="text-align:center;"><a href="."><img src="../media/label/muleviewer.png" alt=""></a><a href="https://'+ siteURL +'/video/stream"><img src="../media/label/quitterspark.png" alt=""></a></span><br>';
+  x.innerHTML = '<span style="text-align:center;"><a href="."><img src="../media/label/muleviewer.png" alt="all my videos" class="animation-bob-in hover-rotate"></a><a href="https://'+ siteURL +'/video/stream"><img src="../media/label/quitterspark.png" alt="my stream" class="animation-bob-in hover-rotate"></a></span><br>';
 }
